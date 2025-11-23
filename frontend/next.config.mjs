e8778da22f3f44f98f98131038bc87d5476a339f
Yash Khope <141517517+Yashkhope01@users.noreply.github.com>
2025-11-23 16:38:09 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // 🚀 Prevent ESLint from breaking your Render build
+    ignoreDuringBuilds: true,
+  },
+  // Required for deploying Next.js on Render
+  output: 'standalone',
+};
 
 export default nextConfig;
