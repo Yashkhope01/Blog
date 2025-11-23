21a2681f5f2e82e9e1209ca122ce8900b5009e65
Yash Khope <141517517+Yashkhope01@users.noreply.github.com>
2025-11-23 16:48:13 +0530

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL ;
 
 // Helper function to get auth token
 const getAuthToken = () => {
